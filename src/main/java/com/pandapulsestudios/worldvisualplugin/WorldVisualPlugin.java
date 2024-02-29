c4bf48f4c79441bc12b8e94236d9bb9a8357ff63
Closeplanet2 <68482902+Closeplanet2@users.noreply.github.com>
2024-02-29 23:17:45 +0000

    Init commit

---
+++ src/main/java/com/pandapulsestudios/worldvisualplugin/WorldVisualPlugin.java
@@ -0,0 +1,17 @@
+package com.pandapulsestudios.worldvisualplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldVisualPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
